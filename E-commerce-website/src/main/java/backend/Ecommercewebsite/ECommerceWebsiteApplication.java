183b0dce855450cf30a372343c627c46c4aa4d1e
Ashima Pal <ashimapal74@gmail.com>
2023-09-07 18:49:04 +0530

    added initial spring project, java version 11, spring version 2.7.15

---
+++ E-commerce-website/src/main/java/backend/Ecommercewebsite/ECommerceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package backend.Ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebsiteApplication.class, args);
+	}
+
+}
